6005b100fe565600b636e9d411bf66b1eecc7be0
Jake <jacobkmiller@icloud.com>
2021-10-03 12:59:42 -0700

    added monterey copy of jtree

---
+++ monterey/destinations.js
@@ -0,0 +1,126 @@
+export const destinations = [
+  // Dakota Haus
+  {
+    // position: new google.maps.LatLng(34.206667, -116.501538),
+    lat: 34.206667,
+    lng: -116.501538,
+    category: 'Lodging',
+    name: 'Dakota Haus',
+    description: '2 beds 2 baths secluded hot tub',
+    address: 'Unknown',
+    // checkIn: '3:00 PM Fri, Feb 7',
+    checkOut: 'Fri, Feb 7',
+    image: 'img/dakotaHaus.jpg'
+    // icon: 'https://static.thenounproject.com/png/5051-200.png',
+  },
+  // Split Rock
+  {
+    // position: new google.maps.LatLng(34.1332342, -116.05992850000001),
+    lat: 34.1332342,
+    lng: -116.05992850000001,
+    category: 'Lodging',
+    name: 'Split Rock',
+    description: 'Near Joshua Tree National Park w/ Spa',
+    address: '6627 Split Rock Ave, Twentynine Palms, CA 92277, USA',
+    checkIn: '3:00 PM Fri, Feb 7',
+    checkOut: '11:00 AM Mon, Feb 10',
+    image: 'img/splitRock.jpg'
+    // icon: 'https://static.thenounproject.com/png/5051-200.png',
+  },
+  // Skull Rock
+  {
+    // position: new google.maps.LatLng(33.998075, -116.059410),
+    lat: 33.998075,
+    lng: -116.059410,
+    category: 'Photography',
+    name: 'Skull Rock',
+    description: '',
+    address: 'Twentynine Palms, CA 92277',
+    icon: 'https://static.thenounproject.com/png/5051-200.png',
+    image: 'img/skullRock.jpeg'
+  },
+  // Arch Rock
+  {
+    // position: new google.maps.LatLng(33.989785, -116.022972),
+    lat: 33.989785,
+    lng: -116.022972,
+    category: 'Photography',
+    name: 'Arch Rock',
+    description: '',
+    address: '33.989785, -116.022972',
+    icon: 'https://cdn3.iconfinder.com/data/icons/minimal-national-park/256/minimal_national_park_1-512.png',
+    image: 'img/archRock.jpeg'
+  },
+  // Jumbo Rocks
+  {
+    // position: new google.maps.LatLng(33.992029, -116.062337),
+    lat: 33.992029,
+    lng: -116.062337,
+    category: 'Photography',
+    name: 'Jumbo Rocks',
+    description: 'Jumbo Rocks Campground',
+    address: 'Twentynine Palms, CA 92277',
+    icon: 'https://cdn3.iconfinder.com/data/icons/nature-life-line-art/128/rock-512.png',
+    image: 'img/jumboRocks.jpeg'
+  },
+  // Barkder Dam
+  {
+    // position: new google.maps.LatLng(34.031035, -116.146322),
+    lat: 34.031035,
+    lng: -116.146322,
+    category: 'Photography',
+    name: 'Barkder Dam',
+    description: 'Dam',
+    address: 'Joshua Tree Parkway, Twentynine Palms, CA 92277',
+    icon: 'https://cdn2.iconfinder.com/data/icons/holidays-one/30/Lakes-512.png',
+    image: 'img/barkderDam.jpeg'
+  },
+  // Cholla Cactus Garden Trail
+  {
+    // position: new google.maps.LatLng(34.031035, -116.146322),
+    lat: 33.925516,
+    lng: -115.928877,
+    category: 'Hike',
+    name: 'Cholla Cactus Garden Trail',
+    description: 'Short hike through cholla cactus, aka "teddy bear" cactus, for its fuzzy-looking (but sharp) spines.',
+    address: '1810 W Orange Ave, El Centro, CA 92243',
+    icon: 'https://cdn1.iconfinder.com/data/icons/cactuses-kawaii-linear-outline/300/205432988-8-512.png',
+    image: 'img/chollaCactusGarden.jpeg'
+  },
+  // Geology Tour Road
+  {
+    // position: new google.maps.LatLng(34.031035, -116.146322),
+    lat: 33.929645,
+    lng: -116.065158,
+    category: 'Drive',
+    name: 'Geology Tour Road',
+    description: '',
+    address: 'Geology Tour Road',
+    icon: 'https://cdn.onlinewebfonts.com/svg/img_546773.png',
+    image: 'img/geologyTour.jpg'
+  },
+  // Keys View
+  {
+    // position: new google.maps.LatLng(34.031035, -116.146322),
+    lat: 33.926843,
+    lng: -116.187215,
+    category: 'Photography',
+    name: 'Keys View',
+    description: `Joshua Tree's highest viewpoint, offering panoramic views of mesas, mountains & Coachella Valley.`,
+    address: 'Salton View Rd Desert Hot Springs, CA 92241',
+    icon: 'https://cdn1.iconfinder.com/data/icons/outdoors-recreation/24/person-outdoor-point-landmarks-recreation-telescope-view-overlook-interest-512.png',
+    image: 'img/keysView.jpeg'
+  },
+  // Wall Street Mill
+  {
+    // position: new google.maps.LatLng(34.031035, -116.146322),
+    lat: 34.036815,
+    lng: -116.133964,
+    category: 'Photography',
+    name: 'Wall Street Mill',
+    description: 'Historical place',
+    address: 'Twentynine Palms, CA 92277',
+    icon: 'https://cdn4.iconfinder.com/data/icons/photo-illustration-categories-1/512/themes_0016-512.png',
+    image: 'img/wallStreetMill.jpg'
+  }
+]
